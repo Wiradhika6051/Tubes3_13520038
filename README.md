bafb5ba6872c486db3123fc15812717c50c8a2a8
Fawwaz Anugrah W D <69834655+Wiradhika6051@users.noreply.github.com>
2022-04-28 22:35:07 +0700

    nambahin backend add penyakit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Tubes3_13520038
 Website "pendeteksi" penyakit menggunakan string matching
+
+butuh install material UI @mui/material
--- src/frontend/src/component/List.js
+++ src/frontend/src/component/List.js
@@ -1,6 +1,64 @@
-import { React, useState } from 'react'
+import { React, useState,useEffect,Component } from 'react'
 import data from './ListData.json'
+import TemplateUrutan from '../pages/TemplateUrutan'
+import axios from 'axios';
+/*
+class List extends Component 
+ {
+    getURL_(props){
+        let url = 'https://localhost:8080/api/search'
+        if(props.length>0){
+            url = url + '/'+ props.replace(/\ /,"%20")
+        }
+        console.log("aaa")
+        return url;
+    }
+    constructor(props){
+        this.state = {
+            query: props,
+            result: [],
+           // url: getURL_(props)
+        };
+        this.getData = this.getData.bind(this)
+        this.getURL  = this.getURL_.bind(this)
+    }
+    getData = () => {
+        axios.get(this.state.query).then(function(response){
+            this.setState({
+                result: response.message
+            })
+        });
+    };
+    //get request
 
+    //create a new array by filtering the original array
+    /*
+    const filteredData = data.filter((el) => {
+        //if no input the return the original
+        if (props.input === '') {
+            return el;
+        }
+        //return the item which contains the user input
+        else {
+            return el.text.toLowerCase().includes(props.input)
+        }
+    })
+    componentDidMount(){
+        this.getData();
+    };
+    render(){
+    return (
+        <ul>
+            {this.state.result.map((item) => (
+                <TemplateUrutan nomor={item.id_tes} waktu={item.tanggal_tes} nama={item.nama} penyakit={item.penyakit} stat={item.status} />
+                //<li key={item.id}>{item.text}</li>
+            ))}
+        </ul>
+    )
+            }
+}
+export default List
+*/
 function List(props) {
     //create a new array by filtering the original array
     const filteredData = data.filter((el) => {
