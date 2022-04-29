11f4d80fb1c8450d68347017761442ff377f018a
shdiqq <71612961+shdiqq@users.noreply.github.com>
2022-04-29 23:20:21 +0700

    Update Penarian.js

--- src/frontend/src/pages/Penarian.js
+++ src/frontend/src/pages/Penarian.js
@@ -1,28 +1,66 @@
 import { React, useState } from "react";
 import { TextField } from "@mui/material";
-import List from "../component/List.js";
 import styles from './Pencarian.module.css'
+import axios from 'axios';
+import swal from 'sweetalert';
 
 function Pencarian() {
-  const [inputText, setInputText] = useState("");
-  let inputHandler = (e) => {
-    //convert input text to lower case
-    var lowerCase = e.target.value.toLowerCase();
-    setInputText(lowerCase);
-  };
+  const [inputText, setInputText] = useState();
+
+  const search = async () => {
+    console.log(inputText, "<= Search");
+    const formData = new FormData();
+    formData.append("inputText", inputText);
+
+    if ( inputText === undefined ) {
+        swal({
+            title: "Failed!",
+            text: "Pencarian Harus diisi",
+            icon: "error",
+            button: "OK"
+        })
+    }
+    else {
+        try {
+            const res = await axios.post("http://localhost:5000/search", formData)
+            console.log(res, '<= res')
+            swal({
+                title: "Success!",
+                text: "Telah berhasil melakukan pencarian",
+                icon: "success",
+                button: "OK"
+            })
+        } catch (error) {
+            console.log(error, "<= error catch search")
+            swal({
+                title: "Failed!",
+                text: "Tidak berhasil melakukan pencarian",
+                icon: "error",
+                button: "OK"
+            })
+        }
+    }
+}
 
   return (
     <div className={styles.main}>
         <h1>Pencarian</h1>
-        <div className={styles.search}>
-            <TextField
-                id="outlined-basic"
-                onChange={inputHandler}
-                variant="outlined"
-                fullWidth
-            />
-        </div>
-        <List input={inputText} />
+        <form>
+          <div className={styles.search}>
+              <TextField
+                  id="outlined-basic"
+                  onChange={event => {
+                    const {value} = event.target;
+                    setInputText(value);
+                  }}
+                  variant="outlined"
+                  fullWidth
+              />
+          </div>
+        </form>
+        <div className={styles.bawah}>
+          <button onClick={search} className={styles.submit}>Search</button>
+      </div>
     </div>
   );
 }
